d3a0cdcfdd2d7cf656e0730f3aa58e448b96a76c
Alexander Bolgov <bolgov1997@mail.ru>
2020-03-29 17:00:49 +0500

    Создан метод для загрузки файла/ов на сервер

--- WebServicesDZ1/Controllers/HomeController.cs
+++ WebServicesDZ1/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebServicesDZ1.Models;
@@ -12,10 +15,12 @@ namespace WebServicesDZ1.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _appEnvironment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment appEnvironment)
         {
             _logger = logger;
+            _appEnvironment = appEnvironment;
         }
 
         public IActionResult Index()
@@ -32,6 +37,21 @@ namespace WebServicesDZ1.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> FileUpload(IFormFileCollection files)
+        {
+            foreach (var uploadedFile in files)
+            {
+                string path = "/Upload/" + uploadedFile.FileName;
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                {
+                    await uploadedFile.CopyToAsync(fileStream);
+                }
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
